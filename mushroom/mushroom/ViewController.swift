b22c5330f28605cb4d3760726f4ce4c210c3caaf
Patrick <patrick.lin@fuhu.com>
2015-06-16 11:03:40 +0800

    add files

---
+++ mushroom/mushroom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mushroom
+//
+//  Created by Patrick@fuhu on 6/16/15.
+//  Copyright (c) 2015 Puyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
