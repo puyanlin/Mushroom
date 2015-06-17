6d3596fd46464444f48214ab34c384b1bb875977
Joseph Lin <puyan.tw@gmail.com>
2015-06-18 02:00:19 +0800

    complete contact

--- mushroom/mushroom/ContactTableViewController.swift
+++ mushroom/mushroom/ContactTableViewController.swift
@@ -10,17 +10,20 @@ import UIKit
 
 class ContactTableViewController: UITableViewController {
 
-    let aboutContact=[["icon":"line","title":"LINE ID","detail":"yenju1208 (香菇）"],
-                      ["icon":"fb","title":"facebook 粉絲專頁","detail":"香菇日韓服飾"],
-                      ["icon":"phone","title":"聯絡電話","detail":"0916-242-077"]]
+    let aboutContact=[["icon":"line","title":"LINE ID","detail":"yenju1208 (香菇）","url":"line://ti/p/l6dsTkb1Bf"],
+                      ["icon":"fb","title":"facebook 粉絲專頁","detail":"香菇日韓服飾","url":"fb://profile/135345183313097"],
+                      ["icon":"phone","title":"聯絡電話","detail":"0916-242-077","url":"telprompt://+886916242077"],
+                      ["icon":"map","title":"位置","detail":"新北市三重區長壽西街54號","url": String("http://maps.apple.com/?q="+"新北市三重區長壽西街54號".stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title="找香菇"
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+         self.clearsSelectionOnViewWillAppear = false
 
+        
+        print("\(aboutContact)")
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
@@ -48,13 +51,23 @@ class ContactTableViewController: UITableViewController {
         }
     }
 
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 66.0
+    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
-        var cell=UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
-
-        // Configure the cell...
-
+        let cell=UITableViewCell(style: indexPath.section == 1 ? UITableViewCellStyle.Value1:UITableViewCellStyle.Subtitle , reuseIdentifier: "Cell")
+        
+        if indexPath.section == 1 {
+            cell.imageView?.image=UIImage(named: aboutContact[indexPath.row]["icon"]!)
+            cell.textLabel?.text=aboutContact[indexPath.row]["title"]
+            cell.detailTextLabel?.text=aboutContact[indexPath.row]["detail"]
+        }else if indexPath.section == 2 {
+            cell.imageView?.image=UIImage(named:"mail")
+            cell.textLabel?.text = "Joseph Lin"
+            cell.detailTextLabel?.text = "puyanlinmailbox@gmail.com"
+        }
+        cell.accessoryType=UITableViewCellAccessoryType.DisclosureIndicator
         return cell
     }
     
@@ -71,6 +84,18 @@ class ContactTableViewController: UITableViewController {
         case 0:     return "我是香菇，因愛穿韓貨、愛看韓劇，因此決定在研究所時候開始我的擺攤人生。沒想到能從邊趕論文邊批貨擺攤到現在全職擺攤生活；更沒想到連粉絲團也成立了，app也有了！！一切都感謝各位的支持愛護，我會更加努力，維持我的熱忱，找更多好看的服飾，讓大家一起變美變有型"+String(UnicodeScalar(0x1F618))
         case 2:     return "Copyright (c) 2015 Puyan.All rights reserved."
         default:    return ""
+        }
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch(indexPath.section){
+        case 0: break
+        default:
+            if indexPath.section == 1 {
+                UIApplication.sharedApplication().openURL(NSURL(string: aboutContact[indexPath.row]["url"]!)!)
+            }else {
+                UIApplication.sharedApplication().openURL(NSURL(string:"mailto://puyanlinmailbox@gmail.com")!)
+            }
         }
     }
     
