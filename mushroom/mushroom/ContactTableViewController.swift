9b8f4dd8371f581aaa9a090505c4e23e9b848632
Joseph Lin <puyan.tw@gmail.com>
2015-06-17 21:44:13 +0800

    modify high-res drawable

--- mushroom/mushroom/ContactTableViewController.swift
+++ mushroom/mushroom/ContactTableViewController.swift
@@ -41,7 +41,11 @@ class ContactTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        switch(section){
+        case 1:     return 4
+        case 2:     return 1
+        default:    return 0
+        }
     }
 
     
@@ -64,7 +68,7 @@ class ContactTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         switch(section){
-        case 0:     return "香菇日韓服飾"
+        case 0:     return "我是香菇，因愛穿韓貨、愛看韓劇，因此決定在研究所時候開始我的擺攤人生。沒想到能從邊趕論文邊批貨擺攤到現在全職擺攤生活；更沒想到連粉絲團也成立了，app也有了！！一切都感謝各位的支持愛護，我會更加努力，維持我的熱忱，找更多好看的服飾，讓大家一起變美變有型"+String(UnicodeScalar(0x1F618))
         case 2:     return "Copyright (c) 2015 Puyan.All rights reserved."
         default:    return ""
         }
